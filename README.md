0b17ab424c48dc15be4e046a3bd65ca22e6ec87d
Anthony <an538811@gmail.com>
2017-10-11 11:41:12 -0500

    abc

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# sequelizeBurgers
+# burger
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (burger_name, devoured) VALUES ('Bacon', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Cheese', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Turkey', false);
